7d6fa98703bb0ddc94a47f24c9c07f422fea5ae9
pqasmi <pmqas002@gmail.com>
2021-09-28 19:22:36 -0400

    added the controller modeule

---
+++ controllers/index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+
+const router = express.Router()
+
+// making an instance of express router
+const BuData1 = require('../models/data')  
+
+router.get('/', (req,res) => {
+    BuData1.find({}, (error, spreadsheet) => {
+        res.render('index.ejs', {
+            data:spreadsheet
+        })
+    })
+    })
+    
+router.get('/new', (req,res) => {
+        res.render('new.ejs')
+    })
+    
+router.get('/:id/edit', (req,res) => {
+            BuData1.findById(req.params.id, (error, record) => {
+                res.render('edit.ejs', {
+                    data:record
+                })
+            })
+       
+    })
+    
+router.get('/:id', (req,res) => {
+        BuData1.findById(req.params.id, (error, record) => {
+            res.render('show.ejs', {
+                data:record
+            })
+        })
+        })
+    
+router.post('/', (req,res) => {
+        BuData1.create(req.body, (error, newSpreadsheet) =>{
+    if(error) {
+        console.log(error)
+    } else {
+        console.log(newSpreadsheet)
+        res.redirect('/index')
+    }
+    
+        })
+    })
+    
+router.delete('/:id', (req, res) => {
+        BuData1.findByIdAndDelete(req.params.id, (error, deletedSpreadsheet) => {
+          // findByIdAndDelete will delete a document with a given id
+          if (error) {
+            console.log(error)
+            res.send(error)
+          } else {
+           // redirect to the index page if the delete successful
+           res.redirect('/index')
+          }
+        })
+      })
+    
+router.put('/:id', (req, res) => {
+        BuData1.findByIdAndUpdate(
+            req.params.id, 
+            req.body, 
+            {
+                new: true,
+              },
+            (error, updateRecord) => {
+          // findByIdAndDelete will delete a document with a given id
+          if (error) {
+            console.log(error)
+            res.send(error)
+          } else {
+           // redirect to the index page if the delete successful
+           res.redirect('/index')
+          }
+        })
+      })
+    
+module.exports = router
+    
--- server.js
+++ server.js
@@ -29,83 +29,15 @@ app.use(methodOverride('_method'))
 app.use(express.static('public'))
 
 //Controllers
-app.get('/index', (req,res) => {
-BuData1.find({}, (error, spreadsheet) => {
-    res.render('index.ejs', {
-        data:spreadsheet
-    })
-})
-})
 
-app.get('/index/new', (req,res) => {
-    res.render('new.ejs')
-})
-
-app.get('/index/:id/edit', (req,res) => {
-        BuData1.findById(req.params.id, (error, record) => {
-            res.render('edit.ejs', {
-                data:record
-            })
-        })
-   
-})
-
-app.get('/index/:id', (req,res) => {
-    BuData1.findById(req.params.id, (error, record) => {
-        res.render('show.ejs', {
-            data:record
-        })
-    })
-    })
-
-app.post('/index', (req,res) => {
-    BuData1.create(req.body, (error, newSpreadsheet) =>{
-if(error) {
-    console.log(error)
-} else {
-    console.log(newSpreadsheet)
-    res.redirect('/index')
-}
-
-    })
-})
-
-app.delete('/index/:id', (req, res) => {
-    BuData1.findByIdAndDelete(req.params.id, (error, deletedSpreadsheet) => {
-      // findByIdAndDelete will delete a document with a given id
-      if (error) {
-        console.log(error)
-        res.send(error)
-      } else {
-       // redirect to the index page if the delete successful
-       res.redirect('/index')
-      }
-    })
-  })
-
-app.put('/index/:id', (req, res) => {
-    BuData1.findByIdAndUpdate(
-        req.params.id, 
-        req.body, 
-        {
-            new: true,
-          },
-        (error, updateRecord) => {
-      // findByIdAndDelete will delete a document with a given id
-      if (error) {
-        console.log(error)
-        res.send(error)
-      } else {
-       // redirect to the index page if the delete successful
-       res.redirect('/index')
-      }
-    })
-  })
+const indexController = require('./controllers/index')
+app.use('/index', indexController)
 
 
 //Listeners
 app.listen(PORT, () => {
     console.log(`Server is connected on PORT: ${PORT}`)
 })
+
 
 
